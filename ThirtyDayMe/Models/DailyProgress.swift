02f9ba1996fa69c9e67924d8d7a808cc27ae8784
pigfyy <111782362+pigfyy@users.noreply.github.com>
2025-09-04 21:29:52 -0700

    Make challenge relationship optional in DailyProgress
    Allows DailyProgress to handle cascade deletion properly when
    parent Challenge is deleted.

--- ThirtyDayMe/Models/DailyProgress.swift
+++ ThirtyDayMe/Models/DailyProgress.swift
@@ -6,7 +6,7 @@ final class DailyProgress {
     @Attribute(.unique) var id: UUID = UUID()
     var date: Date
     var completion: Bool
-    var challenge: Challenge
+    var challenge: Challenge?
     
     init(date: Date, completion: Bool, challenge: Challenge) {
         self.date = date
